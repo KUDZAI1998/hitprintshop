7a61d3d4eb35f1d34116a17b2791c332e9e98137
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2018-03-16 19:52:33 +0200

    Allow user to create a username soon after authenticating with google

--- src/pages/JwtCallback.vue
+++ src/pages/JwtCallback.vue
@@ -1,9 +1,19 @@
 <template>
   <div class="container">
-    <p v-if="authenticated">
-      You have successfully been logged in. <br>
-      <button class="continue-btn"> Continue <v-icon class="white--text">arrow_forward</v-icon> </button>
-    </p>
+    <div v-if="authenticated">
+      <h3 class="title mb-5">You have successfully been logged in.</h3>
+
+      <div v-if="!createdUsername && isNewUser">
+        <p> Now you must create a new username <br>
+          <input type="text" class="form-control" v-model="username" placeholder="username e.g kudapara">
+        </p>
+        <button class="continue-btn" @click="setUsername"> Create Username <v-icon class="white--text">check_circle</v-icon> </button>
+      </div>
+      <div v-else>
+        <p>You have successfully created a username</p>
+        <button class="continue-btn" @click="$router.push('/timeline')"> Continue to app <v-icon class="white--text">arrow_forward</v-icon> </button>
+      </div>
+    </div>
     <p v-if="!authenticated && !attemptedAuthentication">Finishing up the authentication process....</p>
     <p v-if="!authenticated && attemptedAuthentication">
       The system failed to sign you in. <br>
@@ -13,13 +23,15 @@
 </template>
 
 <script>
-import { setUserProfile, setAccessToken } from '@/utils/auth'
+import { setUserProfile, setAccessToken, setTokenObject, getUserProfile, getTokenObject } from '@/utils/auth'
 export default {
   name: 'GoogleCallback',
   data () {
     return {
       authenticated: false,
-      attemptedAuthentication: false
+      attemptedAuthentication: false,
+      username: '',
+      createdUsername: false
     }
   },
   mounted () {
@@ -29,25 +41,56 @@ export default {
       this.attemptedAuthentication = true
       this.authenticated = true
       setUserProfile(response.data.user)
+      setTokenObject(response.data.token)
       setAccessToken(response.data.token.accessToken)
-      this.$router.replace('/timeline')
+      if (!this.isNewUser) this.$router.replace('/timeline')
     }).catch(response => {
       this.attemptedAuthentication = true
     })
+  },
+  methods: {
+    setUsername () {
+      const authHeader = `${getTokenObject().tokenType} ${getTokenObject().accessToken}`
+      this.$http.patch(
+        `/users/${getUserProfile().id}`,
+        { username: this.username },
+        { headers: {
+          Authorization: authHeader
+        }}
+      ).then(response => {
+        const updatedUser = getUserProfile()
+        updatedUser.username = this.username
+        setUserProfile(updatedUser)
+        this.createdUsername = true
+      }).catch(error => {
+        console.log(JSON.stringify(error))
+      })
+    }
+  },
+  computed: {
+    isNewUser () { return this.$route.query.isNewUser === 'true' }
   }
 }
 </script>
 
 <style>
   .container {
-    padding: 40px 0
+    padding: 40px
   }
   .continue-btn {
     padding: 20px;
     background: rgba(16, 16, 235, 0.534);
     border-radius: 3px;
-    width: 200px;
+    min-width: 350px;
     color: white;
     font-size: 18px;
+  }
+  .form-control{
+    border: gray 1px solid;
+    border-radius: 3px;
+    background: #fff;
+    padding: 10px;
+    margin-top: 10px;
+    min-width: 350px;
   }
 </style>
